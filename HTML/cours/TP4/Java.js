7f670228fccbcee446cfdc6f11759975250f6fa1
Mathis Flan <eleve17mathisl@gmail.com>
2025-06-19 13:32:34 +0200

    premier_programme_HTML/CSS/Javascript

---
+++ HTML/cours/TP4/Java.js
@@ -0,0 +1,10 @@
+function onClickChangeTitle() {
+    document.getElementById(id="titre").innerHTML = "Formulaire validé";
+    document.getElementById(id="Prenom").value = "";
+    document.getElementById(id="Nom").value = "";
+    document.getElementById(id="Age").value = "";
+    document.getElementById(id="Email").value = "";
+    document.getElementById(id="Telephone").value = ""
+}
+
+"text contend"
